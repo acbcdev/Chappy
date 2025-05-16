386b589d74609e8642383f8fa4995cd1b80d6a5c
acbc <acbc.dev@gmail.com>
2025-05-16 11:52:36 -0500

    feat: implement chat API route for streaming responses

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,16 @@
+import { streamText } from 'ai';
+import {google} from '@ai-sdk/google'
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: google('gemini-2.0-flash-lite'),
+    system: 'You are a helpful assistant.',
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
