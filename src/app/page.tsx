4ea089f3fb4267e83629b9233ec85680e870ac4e
acbc <acbc.dev@gmail.com>
2025-05-17 20:04:02 -0500

    feat: enhance Home component with chat functionality and improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,35 @@
 "use client";
 
 import { Prompt } from "@/components/chat/prompt";
+import { Messages } from "@/components/messages/messages";
 import { useChat } from "@ai-sdk/react";
+import { toast } from "sonner";
 
 export default function Home() {
-	const { setInput, input, handleSubmit } = useChat({});
+	const { setInput, input, handleSubmit, messages, status, reload } = useChat({
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
 	return (
-		<>
-			<section className="max-w-prose mx-auto">
-				<form onSubmit={handleSubmit}>
-					<Prompt setInput={setInput} input={input} />
+		<section className="@container/main relative flex h-screen flex-col">
+			<div className="flex-1 overflow-y-auto w-full">
+				<Messages messages={messages} onReload={reload} status={status} />
+			</div>
+			<div className="sticky bottom-0 left-0 right-0 bg-background/80 backdrop-blur-md pt-2">
+				<form
+					id="chat-form"
+					className="relative inset-x-0 mx-auto w-full max-w-3xl px-4 pb-4"
+					onSubmit={handleSubmit}
+				>
+					<Prompt
+						setInput={setInput}
+						input={input}
+						status={status}
+						onSend={handleSubmit}
+					/>
 				</form>
-			</section>
-		</>
+			</div>
+		</section>
 	);
 }
