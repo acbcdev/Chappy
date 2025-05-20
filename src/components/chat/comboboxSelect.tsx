82f10ab56abe022f280b0304e3d3f69a43482225
acbc <acbc.dev@gmail.com>
2025-05-19 19:24:14 -0500

    feat: add ComboboxSelect component for model selection with search functionality

---
+++ src/components/chat/comboboxSelect.tsx
@@ -0,0 +1,64 @@
+"use client";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@radix-ui/react-popover";
+import { Button } from "../ui/button";
+import { Check, ChevronsUpDown, Command } from "lucide-react";
+import {
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "../ui/command";
+import { models } from "@/const/models";
+import { useState } from "react";
+
+export function ComboboxSelect() {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="ghost"
+          // biome-ignore lint/a11y/useSemanticElements: <explanation>
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? models.find((model) => model.id === value)?.name
+            : "Select model..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[400px] p-0">
+        <Command className="scrs">
+          <CommandInput placeholder="Search models..." />
+          <CommandList>
+            <CommandEmpty>No model found.</CommandEmpty>
+            <CommandGroup heading="models">
+              {models.map((model) => (
+                <CommandItem
+                  key={model.id}
+                  value={model.id}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue);
+                    setOpen(false);
+                  }}
+                >
+                  {value === model.id && <Check className="mr-2 h-4 w-4" />}
+
+                  {model.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
