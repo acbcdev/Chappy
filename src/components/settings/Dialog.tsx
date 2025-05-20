6e9021fa0bf019c5a7d18ae3afe23ad85636437e
acbc <acbc.dev@gmail.com>
2025-05-20 16:06:32 -0500

    feat: add DialogDemo component for settings management with modal functionality

---
+++ src/components/settings/Dialog.tsx
@@ -0,0 +1,34 @@
+import { useQueryState, parseAsBoolean } from "nuqs";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Cog } from "lucide-react";
+import { SettingsTabs } from "../comp-434";
+
+export function DialogDemo() {
+  const [modalOpen, setModalOpen] = useQueryState(
+    "modalOpen",
+    parseAsBoolean.withDefault(false)
+  );
+  return (
+    <Dialog open={modalOpen} onOpenChange={setModalOpen}>
+      <DialogTrigger asChild>
+        <Button variant="ghost" size={"icon"}>
+          <Cog />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] h-96">
+        <DialogHeader>
+          <DialogTitle>Settings</DialogTitle>
+        </DialogHeader>
+        <SettingsTabs />
+      </DialogContent>
+    </Dialog>
+  );
+}
