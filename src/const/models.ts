f7d7d073e8d7ad168799b34fe25f679b5eea30c7
acbc <acbc.dev@gmail.com>
2025-05-20 19:41:29 -0500

    feat: add active status to providers and update models export to include active state

--- src/const/models.ts
+++ src/const/models.ts
@@ -6,8 +6,12 @@ type modelType = {
 	name: string;
 };
 
-export const providers: Record<providerType, { models: modelType[] }> = {
+export const providers: Record<
+	providerType,
+	{ models: modelType[]; active: boolean }
+> = {
 	openai: {
+		active: true,
 		models: [
 			{ id: "o1", name: "o1" },
 			{ id: "o1-mini", name: "o1 mini" },
@@ -27,6 +31,7 @@ export const providers: Record<providerType, { models: modelType[] }> = {
 		],
 	},
 	google: {
+		active: true,
 		models: [
 			{ id: "gemini-2.0-flash", name: "Gemini 2.0 Flash" },
 			{ id: "gemini-2.0-flash-lite", name: "Gemini 2.0 Flash Lite" },
@@ -52,11 +57,47 @@ export const providers: Record<providerType, { models: modelType[] }> = {
 			},
 		],
 	},
+	anthropic: {
+		active: false,
+		models: [
+			{ id: "claude-3-7-sonnet-20250219", name: "Claude 3.7 Sonnet 20250219" },
+			{ id: "claude-3-5-sonnet-latest", name: "Claude 3.5 Sonnet Latest" },
+			{ id: "claude-3-5-sonnet-20241022", name: "Claude 3.5 Sonnet 20241022" },
+			{ id: "claude-3-5-sonnet-20240620", name: "Claude 3.5 Sonnet 20240620" },
+			{ id: "claude-3-5-haiku-latest", name: "Claude 3.5 Haiku Latest" },
+			{ id: "claude-3-5-haiku-20241022", name: "Claude 3.5 Haiku 20241022" },
+			{ id: "claude-3-opus-latest", name: "Claude 3.0 Opus Latest" },
+			{ id: "claude-3-opus-20240229", name: "Claude 3.0 Opus 20240229" },
+			{ id: "claude-3-sonnet-20240229", name: "Claude 3.0 Sonnet 20240229" },
+			{ id: "claude-3-haiku-20240307", name: "Claude 3.0 Haiku 20240307" },
+		],
+	},
+	mistral: {
+		active: false,
+		models: [
+			{ id: "ministral-3b-latest", name: "Ministral 3B Latest" },
+			{ id: "ministral-8b-latest", name: "Ministral 8B Latest" },
+			{ id: "mistral-large-latest", name: "Mistral Large Latest" },
+			{ id: "mistral-small-latest", name: "Mistral Small Latest" },
+			{ id: "pixtral-large-latest", name: "Pixtral Large Latest" },
+			{ id: "pixtral-12b-2409", name: "Pixtral 12B 2409" },
+			{ id: "open-mistral-7b", name: "Open Mistral 7B" },
+			{ id: "open-mixtral-8x7b", name: "Open Mixtral 8x7B" },
+			{ id: "open-mixtral-8x22b", name: "Open Mixtral 8x22B" },
+		],
+	},
+	xai: {
+		active: false,
+		models: [],
+	},
 };
 
-export const models = Object.entries(providers).flatMap(([provider, value]) =>
-	value.models.map((model) => ({
-		provider,
-		...model,
-	})),
-);
+export const models = Object.entries(providers)
+	.flatMap(([provider, value]) =>
+		value.models.map((model) => ({
+			provider,
+			...model,
+			active: value.active,
+		})),
+	)
+	.filter((models) => models.active);
--- src/const/providres.ts
+++ src/const/providres.ts
@@ -3,15 +3,18 @@ import type { providerType } from "@/types/provider";
 type ProviderT = {
 	name: string;
 	id: providerType;
+	link: string;
 };
 
 export const providers: ProviderT[] = [
 	{
 		name: "OpenAI",
 		id: "openai",
+		link: "https://platform.openai.com/api-keys",
 	},
 	{
 		name: "Google",
 		id: "google",
+		link: "https://aistudio.google.com/apikey",
 	},
 ];
