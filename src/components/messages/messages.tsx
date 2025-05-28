4d918f519445a3498f08ae7cb0249b1a954e979b
acbc <acbc.dev@gmail.com>
2025-05-28 15:43:12 -0500

    feat: add Suggestions component; implement category selection and input handling

--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -1,82 +1,91 @@
 "use client";
 import type { Message as MessageType } from "ai";
 import { Message } from "./message";
-import { useRef } from "react";
+import { type Dispatch, type SetStateAction, useRef } from "react";
 import { PulseDotLoader } from "../ui/loader";
 import { ChatContainer } from "../ui/chat-container";
 import { ScrollButton } from "../ui/scroll-button";
 import { MessageError } from "./messageError";
+import { Suggestions } from "./sugesstions";
 
 type MessagesProps = {
-	messages: MessageType[];
-	onDelete?: (id: string) => void;
-	onEdit?: (id: string, newText: string) => void;
-	onReload: () => void;
-	error?: Error;
-	status: "streaming" | "ready" | "submitted" | "error";
+  messages: MessageType[];
+  onDelete?: (id: string) => void;
+  onEdit?: (id: string, newText: string) => void;
+  onReload: () => void;
+  error?: Error;
+  status: "streaming" | "ready" | "submitted" | "error";
+  setInput: Dispatch<SetStateAction<string>>;
 };
 
-export function Messages({ messages, onReload, status, error }: MessagesProps) {
-	const initialMessageCount = useRef(messages.length);
-	const containerRef = useRef<HTMLDivElement>(null);
+export function Messages({
+  messages,
+  onReload,
+  status,
+  error,
+  setInput,
+}: MessagesProps) {
+  const initialMessageCount = useRef(messages.length);
+  const containerRef = useRef<HTMLDivElement>(null);
 
-	return (
-		<div className="relative flex h-full w-full flex-col items-center overflow-x-hidden overflow-y-auto">
-			<ChatContainer
-				className="relative  flex-1 scrs  overflow-y-auto   flex w-full flex-col gap-y-5 items-center pt-20 pb-4  "
-				autoScroll
-				ref={containerRef}
-				style={{
-					scrollbarGutter: "stable both-edges",
-				}}
-			>
-				{messages.length === 0 && (
-					<div className="absolute bottom-[60%] mx-auto max-w-[50rem] md:relative md:bottom-auto">
-						<h1 className="mb-6 text-3xl font-medium tracking-tight">
-							Ask me anything
-						</h1>
-					</div>
-				)}
+  return (
+    <div className="relative flex h-full w-full flex-col items-center overflow-x-hidden overflow-y-auto">
+      <ChatContainer
+        className="relative  flex-1 scrs  overflow-y-auto   flex w-full flex-col gap-y-5 items-center pt-20 pb-4  "
+        autoScroll
+        ref={containerRef}
+        style={{
+          scrollbarGutter: "stable both-edges",
+        }}
+      >
+        {messages.length === 0 && (
+          <div className="flex w-4xl px-8 max-w-(--breakpoint-md) justify-center flex-col h-1/2 ">
+            <h1 className="mb-6 text-4xl font-medium tracking-tight">
+              How may I help you?
+            </h1>
+            <Suggestions setInput={setInput} />
+          </div>
+        )}
 
-				{messages.map((message, index) => {
-					const isLast = index === messages.length - 1;
-					const hasScrollAnchor =
-						isLast && messages.length > initialMessageCount.current;
-					return (
-						<Message
-							id={message.id}
-							key={message.id}
-							attachments={message.experimental_attachments}
-							isLast={isLast}
-							hasScrollAnchor={hasScrollAnchor}
-							role={message.role}
-							status={status}
-							parts={message.parts}
-							onDelete={() => {}}
-							onEdit={() => {}}
-							onReload={onReload}
-						>
-							{message.content}
-						</Message>
-					);
-				})}
+        {messages.map((message, index) => {
+          const isLast = index === messages.length - 1;
+          const hasScrollAnchor =
+            isLast && messages.length > initialMessageCount.current;
+          return (
+            <Message
+              id={message.id}
+              key={message.id}
+              attachments={message.experimental_attachments}
+              isLast={isLast}
+              hasScrollAnchor={hasScrollAnchor}
+              role={message.role}
+              status={status}
+              parts={message.parts}
+              onDelete={() => {}}
+              onEdit={() => {}}
+              onReload={onReload}
+            >
+              {message.content}
+            </Message>
+          );
+        })}
 
-				{status === "submitted" &&
-					messages.length > 0 &&
-					messages[messages.length - 1].role === "user" && (
-						<div className="group min-h-scroll-anchor flex w-full max-w-3xl flex-col items-start gap-2 px-6 pb-2">
-							<PulseDotLoader />
-						</div>
-					)}
-				{error && <MessageError onReload={onReload} error={error} />}
-			</ChatContainer>
-			<div className="absolute  bottom-0 w-full max-w-3xl">
-				<ScrollButton
-					variant={"secondary"}
-					className="absolute   top-[-50px] right-[45px]"
-					containerRef={containerRef}
-				/>
-			</div>
-		</div>
-	);
+        {status === "submitted" &&
+          messages.length > 0 &&
+          messages[messages.length - 1].role === "user" && (
+            <div className="group min-h-scroll-anchor flex w-full max-w-3xl flex-col items-start gap-2 px-6 pb-2">
+              <PulseDotLoader />
+            </div>
+          )}
+        {error && <MessageError onReload={onReload} error={error} />}
+      </ChatContainer>
+      <div className="absolute  bottom-0 w-full max-w-3xl">
+        <ScrollButton
+          variant={"secondary"}
+          className="absolute   top-[-50px] right-[45px]"
+          containerRef={containerRef}
+        />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/messages/sugesstions.tsx
@@ -0,0 +1,56 @@
+import { PromptSuggestion } from "@/components/ui/prompt-suggestion";
+import { suggestionGroups } from "@/const/suggestions";
+import { Separator } from "@/components/ui/separator";
+import { type Dispatch, type SetStateAction, useState } from "react";
+
+type SuggestionsProps = {
+  setInput: Dispatch<SetStateAction<string>>;
+};
+
+export function Suggestions({ setInput }: SuggestionsProps) {
+  const [activeCategory, setActiveCategory] = useState("");
+  const activeCategoryData = suggestionGroups.find(
+    (group) => group.label === activeCategory
+  );
+
+  return (
+    <div className="relative flex w-full flex-col items-center justify-center space-y-2">
+      <div className="absolute top-0 left-0 space-y-8 h-[70px] w-full">
+        <div className="relative flex w-full flex-wrap  justify-start gap-2">
+          {suggestionGroups.map((suggestion) => (
+            <PromptSuggestion
+              key={suggestion.label}
+              variant={
+                activeCategory === suggestion.label ? "sugest" : "outline"
+              }
+              onClick={() => {
+                setActiveCategory((prev) =>
+                  suggestion.label === prev ? "" : suggestion.label
+                );
+              }}
+              className="capitalize"
+            >
+              <suggestion.Icon />
+              {suggestion.label}
+            </PromptSuggestion>
+          ))}
+        </div>
+        <div className="flex w-full flex-col">
+          {activeCategoryData?.items.map((suggestion) => (
+            <PromptSuggestion
+              key={suggestion}
+              variant="ghost"
+              size={"lg"}
+              highlight={activeCategoryData.highlight}
+              onClick={() => {
+                setInput(suggestion);
+              }}
+            >
+              {suggestion}
+            </PromptSuggestion>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
