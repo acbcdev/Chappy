abc48c5e1631cef975eef0c2f4a1cfee4f0135da
acbc <acbc.dev@gmail.com>
2025-06-07 22:47:01 -0500

    feat: enhance Suggestions component with dynamic loading and additional suggestion categories

--- src/components/messages/sugesstions.tsx
+++ src/components/messages/sugesstions.tsx
@@ -1,14 +1,15 @@
+import { AnimatePresence, motion } from "motion/react";
 import { PromptSuggestion } from "@/components/ui/prompt-suggestion";
 import { suggestionGroups } from "@/const/suggestions";
-import { Separator } from "@/components/ui/separator";
 import { type Dispatch, type SetStateAction, useState } from "react";
-
+import { Minus, Plus } from "lucide-react";
 type SuggestionsProps = {
   setInput: Dispatch<SetStateAction<string>>;
 };
 
 export function Suggestions({ setInput }: SuggestionsProps) {
   const [activeCategory, setActiveCategory] = useState("");
+  const [suggestions, setSuggestions] = useState(suggestionGroups.slice(0, 3));
   const activeCategoryData = suggestionGroups.find(
     (group) => group.label === activeCategory
   );
@@ -17,7 +18,7 @@ export function Suggestions({ setInput }: SuggestionsProps) {
     <div className="relative flex w-full flex-col items-center justify-center space-y-2">
       <div className="absolute top-0 left-0 space-y-8 h-[70px] w-full">
         <div className="relative flex w-full flex-wrap  justify-start gap-2">
-          {suggestionGroups.map((suggestion) => (
+          {suggestions.map((suggestion) => (
             <PromptSuggestion
               key={suggestion.label}
               variant={
@@ -27,6 +28,7 @@ export function Suggestions({ setInput }: SuggestionsProps) {
                 setActiveCategory((prev) =>
                   suggestion.label === prev ? "" : suggestion.label
                 );
+                setInput(suggestion.highlight);
               }}
               className="capitalize"
             >
@@ -34,6 +36,29 @@ export function Suggestions({ setInput }: SuggestionsProps) {
               {suggestion.label}
             </PromptSuggestion>
           ))}
+          <PromptSuggestion
+            variant={"outline"}
+            onClick={() => {
+              setSuggestions((prev) =>
+                prev.length === 3
+                  ? suggestionGroups
+                  : suggestionGroups.slice(0, 3)
+              );
+              if (activeCategory !== "") setActiveCategory("");
+            }}
+          >
+            {suggestions.length === 3 ? (
+              <>
+                <Plus />
+                More
+              </>
+            ) : (
+              <>
+                <Minus />
+                Less
+              </>
+            )}
+          </PromptSuggestion>
         </div>
         <div className="flex w-full flex-col">
           {activeCategoryData?.items.map((suggestion) => (
--- src/const/suggestions.ts
+++ src/const/suggestions.ts
@@ -1,37 +1,120 @@
-import { Code, Frame, GraduationCap } from "lucide-react";
+import {
+  Code,
+  Frame,
+  GraduationCap,
+  Hammer,
+  Languages,
+  MessageCircleQuestion,
+  NotebookPen,
+  NotebookTextIcon,
+  Sparkle,
+} from "lucide-react";
 
-export const suggestionGroups = [
-	{
-		label: "Code",
-		Icon: Code,
-		highlight: "Help me",
-		items: [
-			"Help me write React components",
-			"Help me debug code",
-			"Help me learn Python",
-			"Help me learn SQL",
-		],
-	},
-	{
-		label: "Design",
-		Icon: Frame,
-		highlight: "Design",
-		items: [
-			"Design a small logo",
-			"Design a hero section",
-			"Design a landing page",
-			"Design a social media post",
-		],
-	},
-	{
-		label: "Learn",
-		Icon: GraduationCap,
-		highlight: "Learn",
-		items: [
-			"Learn about React",
-			"Learn about SQL",
-			"Learn about Python",
-			"Learn about the teoria of AI",
-		],
-	},
+type SuggestionCategory = {
+  label: string;
+  Icon: any;
+  highlight: string;
+  items: string[];
+};
+
+export const suggestionGroups: SuggestionCategory[] = [
+  {
+    label: "Code",
+    Icon: Code,
+    highlight: "Help me",
+    items: [
+      "Help me write React components ",
+      "Help me debug code ",
+      "Help me learn Python ",
+      "Help me learn SQL ",
+    ],
+  },
+  {
+    label: "Design",
+    Icon: Frame,
+    highlight: "Design",
+    items: [
+      "Design a small logo ",
+      "Design a hero section ",
+      "Design a landing page ",
+      "Design a social media post ",
+    ],
+  },
+  {
+    label: "Learn",
+    Icon: GraduationCap,
+    highlight: "Learn",
+    items: [
+      "Learn about React ",
+      "Learn about SQL ",
+      "Learn about Python ",
+      "Learn about the teoria of AI ",
+    ],
+  },
+  {
+    label: "Write",
+    Icon: NotebookTextIcon,
+    highlight: "Write",
+    items: [
+      "Write a blog post ",
+      "Write a book ",
+      "Write a report ",
+      "Write a paper ",
+    ],
+  },
+  {
+    label: "Translate",
+    Icon: Languages,
+    highlight: "Translate",
+    items: [
+      "Translate a document ",
+      "Translate a book ",
+      "Translate a poem ",
+      "Translate a this word ",
+    ],
+  },
+  {
+    label: "Inspire",
+    Icon: Sparkle,
+    highlight: "Inspire me",
+    items: [
+      "Inspire me to write a blog post ",
+      "Inspire me to write a book ",
+      "Inspire me to write a report ",
+      "Inspire me to write a paper ",
+    ],
+  },
+
+  {
+    label: "Sumarize",
+    Icon: NotebookPen,
+    highlight: "Sumarize",
+    items: [
+      "Sumarize the French Revolution ",
+      "Sumarize the history of America ",
+      "Sumarize World War I",
+      "Sumarize the benefits of meditation",
+    ],
+  },
+  {
+    label: "Ask",
+    Icon: MessageCircleQuestion,
+    highlight: "Ask",
+    items: [
+      "Ask me a question ",
+      "Ask me a joke ",
+      "Ask me a fact ",
+      "Ask me a poem ",
+    ],
+  },
+  {
+    label: "Create",
+    Icon: Hammer,
+    highlight: "Create",
+    items: [
+      "Create  5 creative writing prompts for flash fiction  ",
+      "Create a magical realism story",
+      "Create a fantasy story",
+    ],
+  },
 ];
